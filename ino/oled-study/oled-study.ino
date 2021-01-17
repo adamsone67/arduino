c9fbf4f28f4cc09662c990dec671ad96094a32d5
Eric Adamson <EricAdamson08@gmail.com>
2021-01-17 16:19:09 -0500

    added barometric pressure & altitude support

--- ino/oled-study/oled-study.ino
+++ ino/oled-study/oled-study.ino
@@ -1,26 +1,35 @@
+#include <Seeed_BMP280.h>
+
 #include <Arduino.h>
 #include <U8x8lib.h>
 #include <dht.h>
 
 #define DHT11_PIN 3
 
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
-dht DHT;
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);  // OLED display
+BMP280 bmp280;                                                      // air pressure sensor
+dht DHT;                                                            // temp/humidity sensor
 
 byte temp = 0;
 byte hum = 0;
+float pressure = 0.0;
 
 void setup(void) {
+  initSensors();
   initDisplay();
 }
 
 void loop(void) {
-  readSensor();
+  readSensors();
   updateDisplay();
   delay(2000);
 }
 
-void readSensor() {
+void initSensors() {
+  bmp280.init();
+}
+
+void readSensors() {
   DHT.read11(DHT11_PIN);
 }
 
@@ -36,5 +45,10 @@ void updateDisplay() {
   u8x8.drawString(6, 0, String((int) fahrenheitTemp).c_str());
   u8x8.drawString(0, 1, " HUM: ");
   u8x8.drawString(6, 1, String((int) DHT.humidity).c_str());
+  u8x8.drawString(0, 2, " PSI: ");
+  pressure = bmp280.getPressure();
+  u8x8.drawString(6, 2, String((pressure / 6895)).c_str());
+  u8x8.drawString(0, 3, " ALT: ");
+  u8x8.drawString(6, 3, String(bmp280.calcAltitude(pressure)).c_str());
   u8x8.refreshDisplay();
 }
